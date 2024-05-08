b74a2fa8edda89ded115b8d02e0009eb5f3dbbe4
YeisonOtalvaro18 <yotalvaroocampo@gmail.com>
2024-05-08 14:45:48 -0500

    Se organizo contacto

--- contacto.js
+++ contacto.js
@@ -1,42 +1,119 @@
-// Capturamos el evento submit del formulario
-document.getElementById('formulario').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evitamos que el formulario se envíe automáticamente
-
-    var nombre = document.getElementById('nombre').value;
-    var apellido = document.getElementById('apellido').value;
-    var correo = document.getElementById('correo').value;
-    var asunto = document.getElementById('asunto').value;
-    var mensaje = document.getElementById('mensaje').value;
-
-    // Validamos los campos
-    if(nombre === '') {
-        mostrarError('nombre', 'Por favor, ingresa tu nombre.');
-        return false;
-    }
-    if(apellido === '') {
-        mostrarError('apellido', 'Por favor, ingresa tu apellido.');
-        return false;
-    }
-    if(correo === '') {
-        mostrarError('correo', 'Por favor, ingresa tu correo.');
-        return false;
-    }
-    if(asunto === '') {
-        mostrarError('asunto', 'Por favor, ingresa un asunto.');
-        return false;
-    }
-    if(mensaje === '') {
-        mostrarError('mensaje', 'Por favor, ingresa un mensaje.');
-        return false;
-    }
-
-    // Si todos los campos están completos, mostramos un mensaje de éxito
-    alert('Formulario enviado con éxito.');
-    // Aquí puedes añadir más validaciones o enviar el formulario al servidor
-});
-
-// Función para mostrar mensajes de error junto a los campos
-function mostrarError(campo, mensaje) {
-    var errorElement = document.getElementById(campo + '-error');
-    errorElement.textContent = mensaje;
-}
+jQuery(document).ready(function($) {
+    "use strict";
+  
+    //Contact
+    $('form.contactForm').submit(function() {
+      var f = $(this).find('.form-group'),
+        ferror = false,
+        emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+  
+      f.children('input').each(function() { // run all inputs
+  
+        var i = $(this); // current input
+        var rule = i.attr('data-rule');
+  
+        if (rule !== undefined) {
+          var ierror = false; // error flag for current input
+          var pos = rule.indexOf(':', 0);
+          if (pos >= 0) {
+            var exp = rule.substr(pos + 1, rule.length);
+            rule = rule.substr(0, pos);
+          } else {
+            rule = rule.substr(pos + 1, rule.length);
+          }
+  
+          switch (rule) {
+            case 'required':
+              if (i.val() === '') {
+                ferror = ierror = true;
+              }
+              break;
+  
+            case 'minlen':
+              if (i.val().length < parseInt(exp)) {
+                ferror = ierror = true;
+              }
+              break;
+  
+            case 'email':
+              if (!emailExp.test(i.val())) {
+                ferror = ierror = true;
+              }
+              break;
+  
+            case 'checked':
+              if (! i.is(':checked')) {
+                ferror = ierror = true;
+              }
+              break;
+  
+            case 'regexp':
+              exp = new RegExp(exp);
+              if (!exp.test(i.val())) {
+                ferror = ierror = true;
+              }
+              break;
+          }
+          i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+        }
+      });
+      f.children('textarea').each(function() { // run all inputs
+  
+        var i = $(this); // current input
+        var rule = i.attr('data-rule');
+  
+        if (rule !== undefined) {
+          var ierror = false; // error flag for current input
+          var pos = rule.indexOf(':', 0);
+          if (pos >= 0) {
+            var exp = rule.substr(pos + 1, rule.length);
+            rule = rule.substr(0, pos);
+          } else {
+            rule = rule.substr(pos + 1, rule.length);
+          }
+  
+          switch (rule) {
+            case 'required':
+              if (i.val() === '') {
+                ferror = ierror = true;
+              }
+              break;
+  
+            case 'minlen':
+              if (i.val().length < parseInt(exp)) {
+                ferror = ierror = true;
+              }
+              break;
+          }
+          i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+        }
+      });
+      if (ferror) return false;
+      else var str = $(this).serialize();
+      var action = $(this).attr('action');
+      if( ! action ) {
+        action = 'contactform/contactform.php';
+      }
+      $.ajax({
+        type: "POST",
+        url: action,
+        data: str,
+        success: function(msg) {
+          // alert(msg);
+          if (msg == 'OK') {
+            $("#sendmessage").addClass("show");
+            $("#errormessage").removeClass("show");
+            $('.contactForm').find("input, textarea").val("");
+          } else {
+            $("#sendmessage").removeClass("show");
+            $("#errormessage").addClass("show");
+            $('#errormessage').html(msg);
+          }
+  
+        }
+      });
+      return false;
+    });
+  
+  });
+  
